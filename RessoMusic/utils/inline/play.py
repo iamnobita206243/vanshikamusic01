dcd3191f9e63b121689215cdd5348f9a169471e8
iamnobita206243 <nobitamusic001@gmail.com>
2025-12-13 23:40:42 +0530

    Update play.py

--- RessoMusic/utils/inline/play.py
+++ RessoMusic/utils/inline/play.py
@@ -52,8 +52,8 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [InlineKeyboardButton("˹ϻʏ ʜᴏϻє˼", url=f"https://t.me/NOBITA_SUPP0RT"),
-         InlineKeyboardButton("˹ᴏᴡɴєʀ˼", url=f"http://t.me/II_YOUR_NOBITA_II"),
+        [InlineKeyboardButton("˹ ϻʏ ʜᴏϻє ˼", url=f"https://t.me/NOBITA_SUPP0RT"),
+         InlineKeyboardButton("˹ ϻʏ ϻᴧsᴛєʀ ˼", url=f"http://t.me/II_YOUR_NOBITA_II"),
 
         ],
         
