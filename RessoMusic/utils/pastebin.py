f45732cb5e9b008bfb51fa5808bb04be518792b1
iamnobita206243 <nobitamusic001@gmail.com>
2026-01-29 00:57:09 +0530

    Update pastebin.py

--- RessoMusic/utils/pastebin.py
+++ RessoMusic/utils/pastebin.py
@@ -2,14 +2,14 @@ import requests
 
 
 class HotyBin:
-    def __init__(self, text: str):
-        self.text = text
+    def __init__(self, content: str):
+        self.content = content
 
     def paste(self):
         try:
             r = requests.post(
                 "https://nekobin.com/api/documents",
-                json={"content": self.text},
+                json={"content": self.content},
                 timeout=10
             )
             if r.status_code == 200:
@@ -17,6 +17,23 @@ class HotyBin:
                 return f"https://nekobin.com/{key}"
         except Exception:
             pass
-
         return "Paste failed"
 
+
+class AMBOTOPBin:
+    def __init__(self, content: str):
+        self.content = content
+
+    def paste(self):
+        try:
+            r = requests.post(
+                "https://nekobin.com/api/documents",
+                json={"content": self.content},
+                timeout=10
+            )
+            if r.status_code == 200:
+                key = r.json()["result"]["key"]
+                return f"https://nekobin.com/{key}"
+        except Exception:
+            pass
+        return "Paste failed"
